474c6fea5db39885843985fa49d689f6f296a5a3
Robert Lin <rlin415@gmail.com>
2017-05-10 17:01:37 -0700

    Implement multiplier in machine language

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,24 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@i
+M=1
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M // D = i - R1
+  @END
+  D;JGT // If (i-R1) > 0 goto END
+  @R0
+  D=M
+  @R2
+  M=D+M // R2 += R0
+  @i
+  M=M+1 // i++
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
